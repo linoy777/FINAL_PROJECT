3d98b9a28a7a0a3190e21e59d8cc924ac8df677f
Shoval Ashkenazi <shovalash1@gmail.comm>
2023-07-14 10:09:51 +0300

    connect to MongoDB

--- index.js
+++ index.js
@@ -5,4 +5,18 @@ http.listen(3000, function () {
     console.log("Server is running on port 3000");
   });
 
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/webDevelopment1", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+
 const port = 3000;
